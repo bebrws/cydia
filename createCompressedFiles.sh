7723c37e80dfbcfbe4e970808cefbb8dc5fb6393
Brad Barrows <bebrws@gmail.com>
2020-04-10 14:26:07 -0700

    Fri Apr 10 14:26:07 PDT 2020

--- createCompressedFiles.sh
+++ createCompressedFiles.sh
@@ -1,7 +1,9 @@
 #!/bin/zsh
+rm Packages.gz   Packages.bz2
 gzip -c9 Packages > Packages.gz
 bzip2 -c9 Packages > Packages.bz2
 
+rm Release.gz   Release.bz2
 gzip -c9 Release > Release.gz
 bzip2 -c9 Release > Release.bz2
 
